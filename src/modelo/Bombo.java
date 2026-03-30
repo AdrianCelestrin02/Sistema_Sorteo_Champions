47c5feda23965b2447cc86393aac81b25a20dd75
Adrián <celestrincorralesadrian@gmail.com>
2026-03-30 18:09:07 +0200

    creacion de clase equipo

--- src/modelo/Bombo.java
+++ src/modelo/Bombo.java
@@ -1,5 +1,32 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Bombo {
+	private ArrayList<Equipo> bombo;
+	
+	public Bombo() {
+		this.bombo=new ArrayList<Equipo>();
+	}
+	
+	public void añadirEquipo(Equipo e) {
+		bombo.add(e);
+	}
+	
+	@SuppressWarnings("null")
+	public Equipo[] elegir2Equipos(Equipo e) {
+		Equipo[] equiposElegidos = null;
+		List<Equipo>listaAuxiliar=new ArrayList<Equipo>();
+		listaAuxiliar.addAll(this.bombo);
+		listaAuxiliar.remove(e);
+		Collections.shuffle(listaAuxiliar);
+		equiposElegidos[0]=listaAuxiliar.get(0);
+		equiposElegidos[1]=listaAuxiliar.get(1);
+		
+		
+		return equiposElegidos;
+	}
 
 }
--- src/modelo/Equipo.java
+++ src/modelo/Equipo.java
@@ -1,5 +1,29 @@
 package modelo;
 
 public class Equipo {
+	private String nombre;
+	private String codigoPais;
+	
+	public Equipo(String nombre, String codigoPais) {
+		super();
+		this.nombre = nombre;
+		this.codigoPais = codigoPais;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+
+     public String getCodigoPais() {
+		return codigoPais;
+	}
+     
+     @Override
+ 	public String toString() {
+ 		return nombre;
+ 	}
+	
 
 }
